b64884af0ff6c44fc2774756c9e6215e3e0f8cae
Juan Luis Ayala Jr <113149815+JuanAyala7@users.noreply.github.com>
2023-02-07 18:40:59 -0600

    Added additional method

--- src/calculator.js
+++ src/calculator.js
@@ -15,3 +15,21 @@ function divide(n1, n2) {
 }
 
 export { add, multiply, subtract, divide };
+
+// const math = {
+//   add: function (n1, n2) {
+//     return n1 + n2;
+//   },
+//   multiply: function (n1, n2) {
+//     return n1 * n2;
+//   },
+//   subtract: function (n1, n2) {
+//     return n1 - n2;
+//   },
+//   divide: function (n1, n2) {
+//     return n1 / n2;
+//   }
+// };
+
+// export default math;
+
