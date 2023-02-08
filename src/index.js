259f83322e9eccdec12fd24180c35266a576b183
Juan Luis Ayala Jr <113149815+JuanAyala7@users.noreply.github.com>
2023-02-07 18:39:18 -0600

    Added additional method

--- src/index.js
+++ src/index.js
@@ -2,10 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import * as calculator from "/src/calculator.js";
 
-//Import the add, multiply, subtract and divide functions
-//from the calculator.js file.
-//If successful, your website should look the same as the Final.png
-
 ReactDOM.render(
   <ul>
     <li>{calculator.add(1, 2)}</li>
@@ -15,3 +11,19 @@ ReactDOM.render(
   </ul>,
   document.getElementById("root")
 );
+
+// Alternative code for the same result: 
+
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import math from "/src/calculator.js";
+
+// ReactDOM.render(
+//   <ul>
+//     <li>{math.add(1, 2)}</li>
+//     <li>{math.multiply(2, 3)}</li>
+//     <li>{math.subtract(7, 2)}</li>
+//     <li>{math.divide(5, 2)}</li>
+//   </ul>,
+//   document.getElementById("root")
+// );
